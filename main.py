6055bb17bbdfee848dcde007f6a9b71426afd96a
Eric Smallwood <smallwood.eric@ymail.com>
2023-12-15 15:38:20 +0800

    change initial image

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from fastai.vision.widgets import *
 azure_key = os.environ.get('AZURE_SEARCH_KEY', 'ef46f0c9d255472b95608666b8288a34')
 
 def test_azure_key():
-    results = search_images_bing(azure_key, 'grizzly bear')
+    results = search_images_bing(azure_key, 'mushroom')
     ims = results.attrgot('contentUrl')
 
     # print length of results to console
     print(len(ims))
 
-    d = 'grizzly.jpg'
+    d = 'mushroom.jpg'
     download_url(ims[0], d)
     image = Image.open(d)
     resized_image = image.to_thumb(128, 128)
